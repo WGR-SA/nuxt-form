631dbafd6234c4c8b8999185ac3c076ae5da7edc
jeanvier <damien.grossfeld@gmail.com>
2023-02-07 00:21:03 +0100

    chore: lint

--- playground/app.vue
+++ playground/app.vue
@@ -1,27 +1,24 @@
 <script setup>
-import { watchEffect } from 'vue'
-import { useState } from '#app'
-
-const selectOptions = {
-  1: 'Option 1',
-  2: 'Option 2',
-  3: 'Option 3'
-}
-
-// watchEffect(() => {
-//   console.log(formResponse.value)
-// })
-
 </script>
 
 <template>
   <div style="max-width: 1200px;padding: 40px;margin: 0 auto;">
     <FormBuilder action="https://httpbin.org/post">
-      <FormInput name="email" label="Email" :rules="['email', 'required']" placeholder="Name" />
+      <FormInput 
+        name="email" 
+        label="Email" 
+        :rules="['email', 'required']" 
+        placeholder="Name" 
+      />
     </FormBuilder>
 
     <FormBuilder action="https://httpbin.org/post">
-      <FormInput name="email" label="Name" :rules="['required']" placeholder="Name" />
+      <FormInput 
+        name="email" 
+        label="Name"   
+        :rules="['required']" 
+        placeholder="Name" 
+      />
     </FormBuilder>
   </div>
 </template>
