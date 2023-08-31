e65d9b2ef5bd7a56766dec1e93491c269efb5a7b
jeanvier <damien.grossfeld@gmail.com>
2023-08-31 09:29:59 +0200

    fix: stop validator if no rule

--- src/runtime/composables/validator.ts
+++ src/runtime/composables/validator.ts
@@ -15,11 +15,15 @@ export const useFormValidator = () => {
     form.validator.rules[field].forEach((rule: any) => {      
       const validator = validators[rule.$params.type as keyof typeof validators]      
 
-      // @ts-ignore TODO: import only validation functions
-      const result = validator(form.data.state[field], rule.$params.options)      
-      
-      if (!result && (form.data.state[field].length > 0 || ['error', 'validate'].includes(form.state.status))) {
-        errors.value.push(rule.$message)
+      if (typeof validator !== 'function') {
+        return
+      }
+      if ( validator ) {
+        // @ts-ignore TODO: import only validation functions
+        const result = validator(form.data.state[field], rule.$params.options)  
+        if (!result && (form.data.state[field].length > 0 || ['error', 'validate'].includes(form.state.status))) {
+          errors.value.push(rule.$message)
+        }
       }
     })
 
