ef4b5149ed27395d207e377805b9aa7617945ee3
jeanvier <damien.grossfeld@gmail.com>
2023-09-20 10:19:21 +0200

    chore: update dep

--- playground/app.vue
+++ playground/app.vue
@@ -1,168 +1,25 @@
 <script setup lang="ts">
-import { ref, watchEffect, computed } from 'vue'
-import { User } from './entities/User'
+import { ref, watchEffect } from 'vue'
 
-const suggestions = [
-  "AMB",
-  "Agrisano",
-  "Aquilana",
-  "Arcosana",
-  "Assura",
-  "Atupri",
-  "Avenir",
-  "Birchmeier",
-  "CSS",
-  "Compact",
-  "Concordia",
-  "EGK",
-  "Easy Sana",
-  "Einsiedeln",
-  "Galenos",
-  "Glarner",
-  "Groupe Mutuel",
-  "Helsana",
-  "Innova",
-  "Intras",
-  "KPT / CPT",
-  "Klug",
-  "Kolping",
-  "Luzerner Hinterland",
-  "Philos",
-  "Provita",
-  "Rhenusana",
-  "SLKK",
-  "Sana24",
-  "Sanagate",
-  "Sanitas",
-  "Sodalis",
-  "Steffisburg",
-  "Sumiswalder",
-  "Supra",
-  "Swica",
-  "Sympany",
-  "Visana",
-  "Visperterminen",
-  "Vita Surselva",
-  "Vivacare",
-  "Wädenswil",
-  "ÖKK"
-] 
-
-const loginForm = ref()
-const userForm = ref()
-
-watchEffect(async () => {
-  if (loginForm.value?.ready()) {
-    const {data, error} = await loginForm.value.actions.submit()
-  }
-
-  if(userForm.value?.ready()){
-    const {data, error} = await userForm.value.actions.submit()
+const testForm = ref()
+watchEffect( async () => {
+  if ( testForm.value?.isReady() ) {
+    testForm.value?.actions.submit()
   }
 })
 
-const newEntity = new User()
-//newEntity.email = 'test@test.ch'
-//newEntity.firstName = 'John'
-newEntity.lastName = 'Doe'
-
-const model = [
-  {
-    name: 'email',
-    label: 'Email',
-    type: 'email',
-    required: true,
-    rules: ['isEmail']
-  },
-  {
-    name: 'firstName',
-    label: 'First Name',
-    type: 'text',
-    required: true,
-    mask: 'A',
-    rules: [{minLength: [3]}]
-  }
-]
 </script>
 
 <template>
   <div style="max-width: 1200px;padding: 40px;margin: 0 auto;">
-    <FormGenerator 
-      ref="userForm"
-      action="https://httpbin.org/post?v=1"
-      :model="User"
-      :values="newEntity"
-      :layers="['typeorm', 'class-validator', 'form']"
+    <FormBuilder
+      ref="testForm"
+      action="https://httpbin.org/post"
     >
+      <FormInput name="asdf" :rules="['isNotEmpty', 'isEmail']" />
+      <FormInput name="safs" :rules="['isNotEmpty']" />
       <FormSubmit>
-        olo
-      </FormSubmit>
-    </FormGenerator>
-
-    <FormGenerator
-      ref="userForm"
-      action="https://httpbin.org/post?v=2"
-      :model="model"
-      :layers="['base']"
-      :messages="{ alert: { submitted: 'Vous vous êtes bien connecté' } }"
-    >
-      <FormSubmit>
-        Login
-      </FormSubmit>
-    </FormGenerator>
-
-    <FormBuilder 
-      ref="loginForm" 
-      action="https://httpbin.org/post?v=3" 
-    >
-      <FormInput 
-        name="assurance2"
-        label="Assurance2"
-        :rules="['isNotEmpty']"
-      />
-      <FormInput 
-        name="assurance"
-        label="Assurance"
-        value="yolo"
-        :readonly="true"
-        :rules="['isNotEmpty']" 
-        :suggestions="{
-          values: suggestions,
-        }"
-      />
-
-
-
-      <!--
-
-      <FormFile
-        name="file" 
-        type="file" 
-        label="file"
-      />
-      <FormInput 
-        mask="A"
-        name="name"
-        label="Nom"
-        :rules="['isNotEmpty']" 
-        placeholder="Nom"
-      /> -->
-      <FormInput 
-        name="username" 
-        label="Email"
-        :required="true"
-        :rules="['isEmail', 'isNotEmpty']" 
-        placeholder="email" 
-      />
-      <!--
-      <FormInput 
-        name="password" 
-        type="password" 
-        label="Password" 
-        placeholder="password" 
-      />  -->
-      <FormSubmit>
-        Login
+        Submit 
       </FormSubmit>
     </FormBuilder>
   </div>
